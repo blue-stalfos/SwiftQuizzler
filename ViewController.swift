85543e5a76601acdec22c499b753412ecad38b52
Richard Batista <36830488+blue-stalfos@users.noreply.github.com>
2019-03-04 22:48:06 -0500

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,65 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let allQuestions = QuestionBank()
+	var pickedAnswer: Bool = false
+	var questionNumber: Int = 0
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var progressBar: UIView!
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+		let firstQuestion = allQuestions.list[0]
+		questionLabel.text = firstQuestion.questionText
+    }
+
+
+    @IBAction func answerPressed(_ sender: AnyObject) {
+		if sender.tag == 1 { pickedAnswer = true }
+		else if sender.tag == 2 { pickedAnswer = false }
+		
+		checkAnswer()
+	
+		questionNumber += 1
+		nextQuestion()
+    }
+    
+    
+    func updateUI() {
+      
+    }
+    
+
+    func nextQuestion() {
+		if questionNumber <= 12 {
+        	questionLabel.text = allQuestions.list[questionNumber].questionText
+		} else {
+			print("End of Quiz")
+			questionNumber = 0
+		}
+	}
+    
+    
+    func checkAnswer() {
+		let correctAnswer = allQuestions.list[questionNumber].answer
+		
+		if correctAnswer == pickedAnswer {
+			print("You got it!")
+		} else {
+			print("Wrong!")
+		}
+    }
+    
+    
+    func startOver() {
+       
+    }
+    
+
+    
+}
